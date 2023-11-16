32e331a67b3c84855f4aad30b69a39ba89b0c3bc
Alberto <74522184+albim360@users.noreply.github.com>
2023-11-16 15:58:27 +0100

    FEAT 3rd TEST

--- createSlug.test.js
+++ createSlug.test.js
@@ -10,3 +10,8 @@ test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
     const result = createSlug('Hello World')
     expect(result).toBe('hello-world');
 });
+
+test('createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -', () => {
+    const result = createSlug('Hello World', []);
+    expect(result).toEqual('hello-world');
+  });
