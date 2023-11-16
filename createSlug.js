3b1954c048a9858b4612a1a39c87eab4d71fd9a3
Alberto <74522184+albim360@users.noreply.github.com>
2023-11-16 16:22:29 +0100

    FEAT 6th TEST

--- createSlug.js
+++ createSlug.js
@@ -1,5 +1,24 @@
-function createSlug (title) {
-    return title.toLowerCase().replace(/\s+/g, '-');
-}
-
-module.exports = createSlug;
+function createSlug(title, posts) {
+    if (!title || typeof title !== 'string') {
+      throw new Error('Il titolo non è presente o ha un formato errato');
+    }
+  
+    if (!posts || !Array.isArray(posts)) {
+      throw new Error('L\'array dei post è mancante');
+    }
+  
+    let baseSlug = title.toLowerCase().replace(/\s+/g, '-');
+    let slug = baseSlug;
+    let count = 1;
+  
+    // Verifica se l'array dei post è definito prima di utilizzarlo
+    while (posts && posts.includes(slug)) {
+      slug = `${baseSlug}-${count}`;
+      count++;
+    }
+  
+    return slug;
+  }
+  
+  module.exports = createSlug;
+  
--- createSlug.test.js
+++ createSlug.test.js
@@ -2,16 +2,35 @@ const { test } = require('@jest/globals');
 const createSlug = require('./createSlug');
 
 test('createSlug dovrebbe ritornare una stringa', () => {
-    const result = createSlug('Hello World');
+    const existingPosts = ['hello-world', 'another-post'];
+    const result = createSlug('Hello World', existingPosts);
     expect(typeof result).toBe('string');
 });
 
 test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
-    const result = createSlug('Hello World')
-    expect(result).toBe('hello-world');
+    const existingPosts = ['hello-world', 'hello-world-1', 'another-post'];
+    const result = createSlug('Hello World', existingPosts);
+    expect(result).toBe('hello-world-2'); 
 });
+
+
 
 test('createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -', () => {
     const result = createSlug('Hello World', []);
     expect(result).toEqual('hello-world');
-  });
+});
+
+test('createSlug dovrebbe incrementare di 1 lo slug quando esiste già', () => {
+    const existingPosts = ['hello-world', 'hello-world-1', 'another-post'];
+    const result = createSlug('Hello World', existingPosts);
+    expect(result).toBe('hello-world-2'); 
+});
+
+test('createSlug dovrebbe lanciare un errore in caso di titolo non presente o formato errato', () => {
+    expect(() => createSlug(null, [])).toThrow('Il titolo non è presente o ha un formato errato');
+});
+
+test('createSlug dovrebbe lanciare un errore se manca l’array dei post', () => {
+    expect(() => createSlug('Hello World')).toThrow('L\'array dei post è mancante');
+});
+
